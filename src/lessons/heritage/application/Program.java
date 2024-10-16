5938f0e1de4a69ce7c5ccc9dfc8e0f33320bbec5
Carlos Vinicius Boehme <carlosviniciusbema@gmail.com>
2024-10-16 16:54:37 -0300

    upcasting and downcasting

--- src/lessons/heritage/application/Program.java
+++ src/lessons/heritage/application/Program.java
@@ -9,8 +9,28 @@ public class Program {
         Account acc = new Account(1, "Carlos", 1000.0);
         BusinessAccount bacc = new BusinessAccount(2, "Vinicius", 500.0, 100.0);
 
+        //UPCASTING
         Account acc1 = bacc;
         Account acc2 = new BusinessAccount(3, "Diego", 500.0, 100.0);
         Account acc3 = new SavingsAccount(4, "Pedro", 500.0, 100.0);
+
+        //DOWNCASTING
+
+        BusinessAccount acc4 = (BusinessAccount) acc2;
+        acc4.loan(500.0);
+
+        //BusinessAccount acc5 = (BusinessAccount) acc3;
+
+        if(acc3 instanceof BusinessAccount){
+            BusinessAccount acc5 = (BusinessAccount) acc3;
+            acc5.loan(200.0);
+            System.out.println("Loan");
+        }
+
+        if(acc3 instanceof SavingsAccount){
+            SavingsAccount acc5 = (SavingsAccount) acc3;
+            acc5.updateBalance();
+            System.out.println("Update");
+        }
     }
 }
